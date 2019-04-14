cc2c59e0606a21b96631aaa11c0889a4dafe3067
toshimaru <me@toshimaru.net>
2019-04-14 16:35:22 +0900

    Add longest_common_prefix

---
+++ longest-common-prefix/ruby.rb
@@ -0,0 +1,17 @@
+def longest_common_prefix(strs)
+    return "" if strs.empty?
+    min = strs.map(&:length).min
+    word = strs.pop
+    a = 0
+    min.times.each { |i|
+        if strs.all? { |w| w[i] == word[i] }
+           a = i + 1
+        else
+            break
+        end
+    }
+    word[0, a]
+end
+
+p longest_common_prefix(["aca","cba"])
+p longest_common_prefix(["a"])
