9d319242450d7ea0060377490d7491ee43f34165
toshimaru <me@toshimaru.net>
2022-02-17 02:15:30 +0900

    Update 21.rb

--- MergeTwoSortedLists/21.rb
+++ MergeTwoSortedLists/21.rb
@@ -1,9 +1,16 @@
+class ListNode
+    attr_accessor :val, :next
+    def initialize(val = 0, _next = nil)
+        @val = val
+        @next = _next
+    end
+end
+
 def merge_two_lists(list1, list2)
     ary = []
-    loop do
+    while list1
         ary << list1.val
         list1 = list1.next
-        break if list1.nil?
     end
     while list2
         ary << list2.val
@@ -12,10 +19,10 @@ def merge_two_lists(list1, list2)
     ary.sort
 end
 
-# def merge_two_lists(list1, list2)
-#     if list1 && list2
-#         if list1.val > list2.val
-#             list1, list2 = list2, list1
-#         list1.next = merge_two_lists(list1.next, list2)
-#     list1 || list2
-# end
+def simple_merge_two_lists(list1, list2)
+    if list1 && list2
+        list1, list2 = list2, list1 if list1.val > list2.val
+        list1.next = merge_two_lists(list1.next, list2)
+    end
+    list1 || list2
+end
