253da1019539823cc3314dc227907510d18dcfb4
toshimaru <me@toshimaru.net>
2022-02-23 13:17:30 +0900

    Update 5.rb

--- LongestPalindromicSubstring/5.rb
+++ LongestPalindromicSubstring/5.rb
@@ -1,15 +1,17 @@
 # @param {String} s
 # @return {String}
 def longest_palindrome(s)
-    return s if s.length == 1
+    l = s.length
+    return s if l == 1
 
     max = 1
     max_i = 0
-    (s.length-1).times do |i|
+    (l-1).times do |i|
         _max = [calc_max(s, i, i+1), calc_max(s, i-1, i+1)].max
         if _max > max
             max = _max
             max_i = i
+            break if max == l # for perforamance
         end
     end
     head = max_i - (max - 1) / 2
@@ -17,12 +19,12 @@ def longest_palindrome(s)
     s[head..tail]
 end
 
-def calc_max(s, b, e)
-    while s[b] == s[e] && b >= 0
-        b -= 1
-        e += 1
+def calc_max(s, l, r)
+    while s[l] == s[r] && l >= 0
+        l -= 1
+        r += 1
     end
-    e - b - 1
+    r - l - 1
 end
 
 s = "cbbd"
