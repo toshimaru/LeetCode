2217a851d40225c9b861c6bb1b563b9abdb26869
toshimaru <me@toshimaru.net>
2022-10-31 04:34:37 +0900

    Add doubly linked list solution

--- design-linked-list/ruby.rb
+++ design-linked-list/ruby.rb
@@ -6,9 +6,9 @@ class Node
         @next = nil
         @prev = nil
     end
-end    
+end
 
-class MyLinkedList
+class MyLinkedList1
     def initialize()
         @head = nil
         @tail = nil
@@ -16,12 +16,9 @@ class MyLinkedList
     end
 
     def get(index)
-        node = @head
-        index.times do
-            return -1 if node.next.nil?
-            node = node.next
-        end
-        node&.val || -1
+        return -1 if index >= @size
+
+        index.times.inject(@head) { |node| node.next }.val
     end
 
     def add_at_head(val)
@@ -50,8 +47,7 @@ class MyLinkedList
             @head = new_node
             @tail = new_node
         else
-            node = @head
-            (index-1).times { node = node.next }
+            node = (index-1).times.inject(@head) { |node| node.next }
             orig_next = node.next
             node.next = new_node
             new_node.next = orig_next
@@ -62,8 +58,7 @@ class MyLinkedList
     def delete_at_index(index)
         return if index >= @size
 
-        node = @head
-        (index-1).times { node = node.next }
+        node = (index-1).times.inject(@head) { |node| node.next }
         if index == 0
             @head = @head.next
         else
@@ -72,19 +67,61 @@ class MyLinkedList
         end
         @size -= 1
     end
+end
+
+class MyLinkedList2
+    def initialize()
+        @dummy = Node.new(nil)
+        @dummy.next = @dummy
+        @dummy.prev = @dummy
+        @size = 0
+    end
+
+    def get(index)
+        return -1 if index >= @size
+
+        get_node(index).val
+    end
+
+    def add_at_head(val)
+        insert_before(@dummy.next, val)
+    end
+
+    def add_at_tail(val)
+        insert_before(@dummy, val)
+    end
+
+    def add_at_index(index, val)
+        return if index > @size
+
+        insert_before(get_node(index), val)
+    end
+
+    def delete_at_index(index)
+        return if index >= @size
+
+        node = get_node(index)
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        @size -= 1
+    end
     
     private
     
-    # for debug
-    def print_node
-        return puts('empty') if @size == 0
-
-        ary = [@head.val]
-        node = @head
-        while node.next
-            ary << node.next.val
-            node = node.next
+    def get_node(index)
+        if index > @size / 2 
+            (@size - index).times.inject(@dummy) { |node| node.prev }
+        else
+            index.times.inject(@dummy.next) { |node| node.next }
         end
-        puts ary.join('->')
+    end
+    
+    def insert_before(node, val)
+        new_node = Node.new(val)
+        new_node.prev = node.prev
+        new_node.next = node
+        node.prev = new_node
+        new_node.prev.next = new_node
+        @size += 1
     end
 end
