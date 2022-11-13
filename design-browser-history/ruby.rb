2b1bedd39418205794a5b8386687432f6b50c75a
toshimaru <me@toshimaru.net>
2022-11-13 10:42:06 +0900

    BrowserHistory with DoublyLinkedList

--- design-browser-history/ruby.rb
+++ design-browser-history/ruby.rb
@@ -1,4 +1,5 @@
-class BrowserHistory
+# history using stack
+class BrowserHistoryStack
     def initialize(homepage)
         @history = [homepage]
         @p_history = []
@@ -19,5 +20,39 @@ class BrowserHistory
         steps = @p_history.length if steps > @p_history.length
         steps.times { @history << @p_history.pop }
         @history[-1]
+    end
+end
+
+# history using Doubly Linked List
+class Node
+    attr_accessor :val, :prev, :next
+
+    def initialize(val)
+        @val = val
+        @prev = nil
+        @next = nil
+    end
+end
+
+class BrowserHistory
+    def initialize(homepage)
+        @root = Node.new(homepage)
+    end
+
+    def visit(url)
+        node = Node.new(url)
+        node.prev = @root
+        @root.next = node
+        @root = node
+    end
+
+    def back(steps)
+        steps.times { break unless @root.prev; @root = @root.prev }
+        @root.val
+    end
+
+    def forward(steps)
+        steps.times { break unless @root.next; @root = @root.next }
+        @root.val
     end
 end
