9329113158ba7a3eb6ac780b0f3c56e84969ede5
toshimaru <me@toshimaru.net>
2023-07-09 17:49:27 +0900

    226. Invert Binary Tree

---
+++ invert-binary-tree/ruby.rb
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+    return if root.nil?
+    root.right, root.left = invert_tree(root.left), invert_tree(root.right)
+    root
+end
