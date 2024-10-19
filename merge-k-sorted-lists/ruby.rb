d7ab749f827eba7c9db655175ddad5ac8df1c7c2
toshimaru <me@toshimaru.net>
2024-10-19 10:23:46 +0900

    More ruby-ish code

--- merge-k-sorted-lists/ruby.rb
+++ merge-k-sorted-lists/ruby.rb
@@ -22,37 +22,26 @@
 # end
 
 def merge_k_lists(lists)
-  return if lists.size == 0
-
-  while lists.size > 1
-    merged_lists = []
-    (0..lists.size-1).step(2) do |i|
-      l1 = lists[i]
-      l2 = lists[i+1]
-      merged_lists << merge_lists(l1, l2)
+    while lists.size > 1
+        lists = lists.each_slice(2).inject([]) { |memo, (a, b)| memo << merge(a, b) }
     end
-    lists = merged_lists
-  end
-
-  lists[0]
+    lists.first
 end
 
-def merge_lists(l1, l2)
-  dummy = ListNode.new
-  tail = dummy
-
-  while l1 && l2
-    if l1.val < l2.val
-      tail.next = l1
-      l1 = l1.next
-    else
-      tail.next = l2
-      l2 = l2.next
+def merge(a, b)
+    dummy = ListNode.new
+    tail = dummy
+    while a && b
+        if a.val < b.val
+            tail.next = a
+            a = a.next
+        else
+            tail.next = b
+            b = b.next
+        end
+        tail = tail.next
     end
-    tail = tail.next
-  end
-  tail.next = l1 if l1
-  tail.next = l2 if l2
-
-  dummy.next
+    tail.next = a if a
+    tail.next = b if b
+    dummy.next
 end
