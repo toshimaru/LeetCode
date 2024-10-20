640d7e51ad4ab4e25f7fc8aede3ff8bec51837e0
toshimaru <me@toshimaru.net>
2024-10-20 16:43:49 +0900

    Update ruby.rb

--- two-sum/ruby.rb
+++ two-sum/ruby.rb
@@ -1,27 +1,27 @@
-def two_sum(nums, target)
+# !Time Limit Exceeded!
+def two_sum1(nums, target)
     answer = []
     nums.count.times { |n1|
         nums.count.times { |n2|
             next if n1 == n2
-            answer = [n1, n2] and break if nums[n1] + nums[n2] == target
+            return [n1, n2] if nums[n1] + nums[n2] == target
         }
-        break answer unless answer.empty?
     }
 end
 
 def two_sum2(nums, target)
     nums.count.times { |n|
         if index = nums.index(target - nums[n])
-            break [n, index] unless n == index
+            return [n, index] unless n == index
         end
     }
 end
 
-def two_sum3(nums, target)
+def two_sum(nums, target)
     hash = {}
-    nums.each.with_index { |num, i|
+    nums.each_with_index { |num, i|
         if val = hash[num]
-            break [val, i]
+            return [val, i]
         else
             hash[target - num] = i
         end
@@ -29,5 +29,5 @@ def two_sum3(nums, target)
 end
 
 p two_sum([11, 15, 2, 7], 9)
+p two_sum1([11, 15, 2, 7], 9)
 p two_sum2([11, 15, 2, 7], 9)
-p two_sum3([11, 15, 2, 7], 9)
