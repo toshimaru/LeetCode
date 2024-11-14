18938d5c94aee986c0602e6d19a697c5e139f954
toshimaru <me@toshimaru.net>
2024-11-15 02:49:13 +0900

    Create binary-tree-inorder-traversal/ruby.rb

---
+++ binary-tree-inorder-traversal/ruby.rb
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+
+# Solution 1
+def inorder_traversal(root)
+    ary = []
+    traversal(root, ary)
+    ary
+end
+
+def traversal(root, ary)
+    return if root.nil?
+    traversal(root.left, ary)
+    ary << root.val
+    traversal(root.right, ary)
+end
+
+# Solution 2
+def inorder_traversal(root)
+    return [] if root.nil?
+
+    ary = []
+    ary += inorder_traversal(root.left) if root.left
+    ary << root.val
+    ary += inorder_traversal(root.right) if root.right
+    ary
+end
