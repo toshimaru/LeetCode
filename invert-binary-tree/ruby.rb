69c4b98f67d8762fd951132e470acad0cac9e9c6
toshimaru <me@toshimaru.net>
2025-02-09 18:13:11 +0900

    Create invert-binary-tree/ruby.rb

--- invert-binary-tree/ruby.rb
+++ invert-binary-tree/ruby.rb
@@ -9,8 +9,24 @@
 # end
 # @param {TreeNode} root
 # @return {TreeNode}
+# DSF
 def invert_tree(root)
     return if root.nil?
     root.right, root.left = invert_tree(root.left), invert_tree(root.right)
+    root
+end
+
+# BSF
+def invert_tree(root)
+    return if root.nil?
+
+    queue = [root]
+    while queue.any?
+        node = queue.shift
+        node.left, node.right = node.right, node.left
+        queue << node.left if node.left
+        queue << node.right if node.right
+    end
+
     root
 end
