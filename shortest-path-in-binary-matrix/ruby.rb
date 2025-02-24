9095a71bda6f73a71fad41c65085c57c58c6398f
toshimaru <me@toshimaru.net>
2025-02-25 01:28:55 +0900

    another solution

--- shortest-path-in-binary-matrix/ruby.rb
+++ shortest-path-in-binary-matrix/ruby.rb
@@ -30,3 +30,25 @@ def shortest_path_binary_matrix(grid)
     -1
 end
 
+# 100% solution
+def shortest_path_binary_matrix2(grid)
+    n = grid.size
+    return -1 if grid[0][0] > 0 || grid[n-1][n-1] > 0
+
+    q = [[0, 0, 1]]
+    grid[0][0] = 1
+    while q.any?
+        r, c, len = q.shift
+        return len if r == n-1 && c == n-1
+        DIRECTIONS.each do |dr, dc|
+            nr, nc = r+dr, c+dc
+            if nr >= 0 && nc >= 0 && nr < n && nc < n && grid[nr][nc] == 0
+                grid[nr][nc] = 1
+                q.push([nr, nc, len+1])
+            end
+        end
+    end
+
+    -1
+end
+
